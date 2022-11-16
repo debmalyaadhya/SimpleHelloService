e44b7476d3da49fbd0cec4dda4513c4f34609deb
debmalya <deb11007@gmail.com>
2022-11-16 15:08:27 +0000

    Initial commit new project

---
+++ src/main/java/com/simplemicroservice/SimpleHelloService/SimpleHelloServiceApplication.java
@@ -0,0 +1,13 @@
+package com.simplemicroservice.SimpleHelloService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHelloServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHelloServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplemicroservice/SimpleHelloService/SimpleHelloServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplemicroservice.SimpleHelloService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleHelloServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
